67cfa0dbb927ec2623bad73aba3e337057caba39
AlexGCprint.ru <dir@gcprint.ru>
2020-12-14 01:39:24 +0300

    l6

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from mainapp.models import Product, ProductCategory
 from django.conf import settings
+import datetime
+import random
 
 from basketapp.models import Basket
+
+def get_basket(user):
+    if user.is_authenticated:
+        return Basket.objects.filter(user=user)
+    return []
+
+def get_hot_product():
+    products_list = Product.objects.all()
+    return random.sample(list(products_list), 1)[0]
+
+def get_same_products(hot_product):
+    return Product.objects.filter(category__pk=hot_product.category.pk).exclude(pk=hot_product.pk)[:3]
 
 
 def main(request):
@@ -11,18 +25,15 @@ def main(request):
     products = Product.objects.all()[:4]
     content ={
         'title': title,
-        'products': products
+        'products': products,
+        'basket': get_basket(request.user),
     }
     return render(request, 'mainapp/index.html', content)
 
 def products(request, pk=None):
     title = 'продукты'
     links_menu = ProductCategory.objects.all()
-
-    basket = []
-    if request.user.is_authenticated:
-        basket = Basket.objects.filter(user=request.user)
-
+    basket = get_basket(request.user)
 
     if pk is not None:
         if pk == 0:
@@ -37,28 +48,67 @@ def products(request, pk=None):
             "links_menu": links_menu,
             'products': products_list,
             'category': category,
-            'basket': basket
+            'basket': basket,
+            'hot_product': get_hot_product(),
         }
 
         return render(request, "mainapp/products_list.html", content)
 
-    same_products = Product.objects.all()
+    hot_product = get_hot_product()
+
+    same_products = get_same_products(hot_product)
 
     content = {
         "title": title,
         "links_menu": links_menu,
         "same_products": same_products,
         "media_url": settings.MEDIA_URL,
-        "basket": basket,
+        'basket': basket,
+        'hot_product': get_hot_product(),
     }
     if pk:
         print(f"User select category: {pk}")
 
     return render(request, "mainapp/products.html", content)
 
+def product(request, pk):
+    title = 'продукты'
+    content = {
+        'title': title,
+        'links_menu': ProductCategory.objects.all(),
+        'product': get_object_or_404(Product, pk=pk),
+        'basket': get_basket(request.user),
+    }
+
+    return render(request,  "mainapp/product.html", content)
+
 def contact(request):
     title = 'Контакты'
-    content ={
-        'title': title
+    visit_date = datetime.datetime.now()
+    locations = [
+        {
+            'city': 'Москва',
+            'phone': '+7-888-888-8888',
+            'email': 'info@geekshop.ru',
+            'adress': 'В пределах МКАД',
+        },
+        {
+            'city': 'Екатеринбург',
+            'phone': '+7-888-888-8888',
+            'email': 'info@geekshop.ru',
+            'adress': 'Близко к центру',
+        },
+        {
+            'city': 'Владивосток',
+            'phone': '+7-888-888-8888',
+            'email': 'info@geekshop.ru',
+            'adress': 'Близко к океану',
+        },
+    ]
+    content = {
+        'title': title,
+        'visit_date': visit_date,
+        'locations': locations,
+        'basket': get_basket(request.user),
     }
     return render(request, 'mainapp/contact.html', content)
